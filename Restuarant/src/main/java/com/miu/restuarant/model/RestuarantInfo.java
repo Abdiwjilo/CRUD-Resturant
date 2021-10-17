b1c8d8878b1ef104be3456a49ae35ac7de5bf66f
Abdi W. Jilo <89826434+Abdiwjilo@users.noreply.github.com>
2021-10-17 15:29:50 -0500

    Add files via upload

---
+++ Restuarant/src/main/java/com/miu/restuarant/model/RestuarantInfo.java
@@ -0,0 +1,20 @@
+package com.miu.restuarant.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@ToString
+
+@Document(collation = "RestuarantInfo")
+public class RestuarantInfo {
+    @Id
+    private int id;
+    private String title;
+    private String location;
+    private String description;
+}
---
+++ Restuarant/src/main/java/com/miu/restuarant/resource/RestuarantInfoController.java
@@ -0,0 +1,46 @@
+package com.miu.restuarant.resource;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miu.restuarant.model.RestuarantInfo;
+import com.miu.restuarant.repository.RestuarantInfoRepository;
+
+@RestController
+public class RestuarantInfoController {
+
+    @Autowired
+    private RestuarantInfoRepository repository;
+
+    @PostMapping("/addrestuarantInfo")
+    public String saverestuarantInfo(@RequestBody RestuarantInfo restuarantInfo) {
+        repository.save(restuarantInfo);
+        return "Added restuarantInfo with id : ";
+    }
+
+    @GetMapping("/findAllrestuarantInfos")
+    public List<RestuarantInfo> getrestuarantInfos() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/findAllrestuarantInfo/{id}")
+    public Optional<RestuarantInfo> getrestuarantInfo(@PathVariable int id) {
+        return repository.findById(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleterestuarantInfo(@PathVariable int id) {
+        repository.deleteById(id);
+        return "restuarantInfo deleted with id : " + id;
+    }
+
+}
